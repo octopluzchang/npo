9d341fbb6cee810ee71c9e50d0f5e907a24fb90e
octopluzchang <ckenny0215@gmail.com>
2015-02-02 21:54:24 +0800

    example for jeremy
    show you how to update files

---
+++ user_jeremy.php
@@ -0,0 +1,15 @@
+
+<ul class="nav navbar-nav navbar-right">
+        <li><img data-src="holder.js/40x40/text:J" class="img-circle"></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Jeremy Lin<span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">正在進行的專案</a></li>
+            <li><a href="#">已完成的專案</a></li>
+            
+            <li class="divider"></li>
+            <li><a href="#">個人資料設定</a></li>
+            <li><a href="#">登出帳號</a></li>
+          </ul>
+        </li>
+      </ul>
