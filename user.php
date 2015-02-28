70b434d3a8a79310e66759ec05b0f3d86f777221
octopluzchang <ckenny0215@gmail.com>
2015-02-28 16:39:45 +0800

    change dropdown menu behavior

--- user.php
+++ user.php
@@ -6,18 +6,25 @@ $query_RecMember = "SELECT * FROM `memberdata` WHERE `m_username`='" . $_SESSION
 $RecMember = mysql_query($query_RecMember);
 $row_RecMember = mysql_fetch_assoc($RecMember);
 ?>
-<ul class="nav navbar-nav navbar-right">
-        <li><img data-src="holder.js/40x40/text:K" class="img-circle"></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?php echo $row_RecMember["m_username"];?><span class="caret"></span></a>
+<div class="nav navbar-nav navbar-right navbar-btn">
+        <div class="btn-group">
+        	<button type="button" class="btn"><a href="profile.php"><?php echo $row_RecMember["m_username"];?></a></button>
+            <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+    		<span class="caret"></span>
+    			<span class="sr-only">Toggle Dropdown</span>
+  			</button>
+          
+          
           <ul class="dropdown-menu" role="menu">
             <li><a href="profile.php">正在進行的專案</a></li>
             <li><a href="profile.php">已完成的專案</a></li>
             <li><a href="profile.php">追蹤中的專案的專案</a></li>
             
             <li class="divider"></li>
-            <li><a href="member_update.php">個人資料設定</a></li>
             <li><a href="?logout=true">登出帳號</a></li>
           </ul>
-        </li>
-      </ul>
+        </div>
+      </div>
+      
+      
+      
