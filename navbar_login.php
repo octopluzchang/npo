5d6f8a6db4ecd71b2ab2253fb6cdcaae0101352a
octopluzchang <ckenny0215@gmail.com>
2015-06-14 14:17:20 +0800

    update

--- navbar_login.php
+++ navbar_login.php
@@ -1,39 +1,39 @@
 <nav class="navbar  navbar-default">
-	<div class="container">
-		<div class="navbar-header">
-			<div class="btn-group navbar-btn">
-				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-					NPO LAB <span class="caret"></span>
-				</button>
-				<ul class="dropdown-menu" role="menu">
-					<li>
-						<a href="member_center.php">回到首頁</a>
-					</li>
-					<li>
-						<a href="#">關於我們</a>
-					</li>
-					<li class="divider"></li>
-					<li>
-						<a href="#">獎杯室</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-		<form name="search" id="search" action="result.php" method="post" class="navbar-form navbar-left" role="search" onSubmit="return checkForm();" >
-			<div class="input-group">
-				<span class="input-group-addon" id="basic-addon2"> <span class="label label-info input">設計 <span class="glyphicon glyphicon-remove"></span></span> <span class="label label-info">行銷 <span class="glyphicon glyphicon-remove"></span></span> <span class="label label-info">網站 <span class="glyphicon glyphicon-remove"></span></span> </span><span class="input-group-addon" id="basic-addon2"> <a data-toggle="modal" data-target="#filterModal"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>新增條件</a> </span>
-				<input name="input" type="text" class="form-control" placeholder="請輸入專案名或張貼人" aria-describedby="basic-addon2">
-				<input name="type" type="hidden" value="xxxx" />
-				<!-- 隱藏數值 -->
-			</div>
-			<input name="ok" class="btn btn-info" type="submit" value="搜尋" />
-			<input name="retype" class="btn btn-info" type="reset" value="重設" />
-		</form>
-		<div id="userStatus">
-			<?php
-			include ("user.php");
-			?>
-		</div>
+    <div class="container">
+        <div class="navbar-header">
+            <div class="btn-group navbar-btn">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    NPO LAB <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a href="member_center.php">回到首頁</a>
+                    </li>
+                    <li>
+                        <a href="#">關於我們</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="#">獎杯室</a>
+                    </li>
+                </ul>
+            </div>
+        </div><!--
+        <form name="search" id="search" action="result.php" method="post" class="navbar-form navbar-left" role="search" onSubmit="return checkForm();">
+            <div class="input-group">
+                <span class="input-group-addon" id="basic-addon2"> <span class="label label-info input">設計 <span class="glyphicon glyphicon-remove"></span></span> <span class="label label-info">行銷 <span class="glyphicon glyphicon-remove"></span></span> <span class="label label-info">網站 <span class="glyphicon glyphicon-remove"></span></span>
+                </span><span class="input-group-addon" id="basic-addon2"> <a data-toggle="modal" data-target="#filterModal"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>新增條件</a>
+                </span>
+                <input name="input" type="text" class="form-control" placeholder="請輸入專案名或張貼人" aria-describedby="basic-addon2">
+                <input name="type" type="hidden" value="xxxx" />
+                
+            </div>
+            <input name="ok" class="btn btn-info" type="submit" value="搜尋" />
+            <input name="retype" class="btn btn-info" type="reset" value="重設" />
+        </form>-->
+        <div id="userStatus">
+            <?php include ( "user.php"); ?>
+        </div>
 
-		<!-- /.container -->
+        <!-- /.container -->
 </nav>
